b4926ab61390e9a03dec0f1e24574bc9d78f084c
Julian Lees <julian.j.lees@gmail.com>
2017-11-09 23:39:47 +1300

    Crickemo progress day 1

--- app/src/main/java/com/example/julia/crickemov2/CrickeMoMain.java
+++ app/src/main/java/com/example/julia/crickemov2/CrickeMoMain.java
@@ -2,12 +2,45 @@ package com.example.julia.crickemov2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class CrickeMoMain extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+public class CrickeMoMain extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,GoogleMap.OnMarkerClickListener {
+
+    private static final String TAG = CrickeMoMain.class.getSimpleName();
+
+    private TextView textLat, textLong;
+    private MapFragment mapFragment;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cricke_mo_main);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        return false;
     }
 }
