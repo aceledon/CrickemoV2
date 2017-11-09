fc41893956b0c2139c5b710289f1702e6e5928a9
Julian Lees <julian.j.lees@gmail.com>
2017-11-10 10:17:08 +1300

    Crickemo progress AM Day 2

--- app/src/main/java/com/example/julia/crickemov2/CrickeMoMain.java
+++ app/src/main/java/com/example/julia/crickemov2/CrickeMoMain.java
@@ -1,46 +1,277 @@
 package com.example.julia.crickemov2;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class CrickeMoMain extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener,GoogleMap.OnMarkerClickListener {
+public class CrickeMoMain extends AppCompatActivity
+        implements
+        OnMapReadyCallback,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private static final String TAG = CrickeMoMain.class.getSimpleName();
+    //Location updates every 5 seconds
+    private final int UPDATE_INTERVAL = 5000;
+    private final int FASTEST_INTERVAL = 5000;
+    private final int REQ_PERMISSION = 101;
 
     private TextView textLat, textLong;
     private MapFragment mapFragment;
     private GoogleMap map;
+    private GoogleApiClient googleApiClient;
+    private Location lastLocation;
+    private LocationRequest locationRequest;
+    //Marker for the devices current location
+    private Marker locationMarker;
+    //Marker for the geoFence (i.e. the location of interest
+    private Marker geoFenceMarker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cricke_mo_main);
+        textLat = (TextView) findViewById(R.id.lat);
+        textLong = (TextView) findViewById(R.id.lon);
+
+        //Initialise GoogleMaps
+        initGMaps();
+
+        //Create GoogleApiClient
+        createGoogleApi();
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Call GoogleApiClient connection when starting the Activity
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Disconnect GoogleApiClient when stopping Activity
+        googleApiClient.disconnect();
+    }
+
+    //Method to initialize GoogleMaps
+    private void initGMaps() {
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    //Method to create the GoogleApiClient instance
+    private void createGoogleApi() {
+        Log.d(TAG, "createGoogleApi()");
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
+    // Callback function which is called when the Map is ready
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
+        Log.d(TAG, "onMapReady()");
+        map = googleMap;
+        map.setOnMapClickListener(this);
+        map.setOnMarkerClickListener(this);
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
+    //Callback called when Map is touched
     @Override
     public void onMapClick(LatLng latLng) {
+        Log.d(TAG, "onMapClick(" + latLng + ")");
+        markerForGeofence(latLng);
+    }
 
+    //Callback called when Marker is touched
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Log.d(TAG, "onMarkerClickListener: " + marker.getPosition());
+        return false;
+    }
+
+    //GoogleApiClient.ConnectionCallbacks connected
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.i(TAG, "onConnected()");
+        getLastKnownLocation();
+    }
+
+    //GoogleApiClient.ConnectionCallbacks suspended
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.w(TAG, "onConnectionSuspended");
+    }
+
+    //GoogleApiClient.OnConnectionFailedListener fail
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.w(TAG, "onConnectionFailed()");
+    }
+
+    //Method to retrieve the last known location
+    private void getLastKnownLocation() {
+        Log.d(TAG, "getLastKnownLocation()");
+        if ( checkPermission()) {
+            lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+            if (lastLocation != null) {
+                Log.i(TAG, "Last Known Location. " + "Long: " + lastLocation.getLongitude() + " | Lat: " + lastLocation.getLatitude());
+                writeLastLocation();
+                startLocationUpdates();
+            } else {
+                Log.w(TAG, "No location retrieved yet");
+                startLocationUpdates();
+            }
+
+        } else {
+            askPermission();
+        }
+    }
+
+    private void startLocationUpdates() {
+        Log.i(TAG, "startLocationUpdated()");
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+
+        if (checkPermission()) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+        }
     }
 
     @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
+    public void onLocationChanged(Location location) {
+        Log.i(TAG, "onLocaitonChanged [" + location + "]");
+        lastLocation = location;
+        writeActualLocation(location);
+    }
+
+    //Method to write the location Lat/Lon to the UI
+    private void writeActualLocation(Location location) {
+        textLat.setText("Lat: " + location.getLatitude());
+        textLong.setText("Long: " + location.getLongitude());
+        markerLocation(new LatLng(location.getLatitude(), location.getLongitude()));
+    }
+
+    private void writeLastLocation() {
+        writeActualLocation(lastLocation);
+    }
+
+    private void markerLocation(LatLng latLng) {
+        Log.i(TAG, "markerLocation(" + latLng + ")");
+        String title = latLng.latitude + ", " + latLng.longitude;
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .title(title);
+
+        if (map != null) {
+            //Remove the anterior marker
+            if (locationMarker != null) {
+                locationMarker.remove();
+            }
+            locationMarker = map.addMarker(markerOptions);
+            float zoom = 14f;
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
+            map.animateCamera(cameraUpdate);
+        }
+    }
+
+    private void markerForGeofence(LatLng latLng) {
+        Log.i(TAG, "markerForGeofence(" + latLng + ")");
+        String title = latLng.latitude + ", " + latLng.longitude;
+
+        //Define marker options
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .title(title);
+
+        if (map != null) {
+            //Remove last geoFenceMarker
+            if (geoFenceMarker != null) {
+                geoFenceMarker.remove();
+            }
+
+            geoFenceMarker = map.addMarker(markerOptions);
+        }
+    }
+
+    //Method to check permission to access location
+    private boolean checkPermission() {
+        Log.d(TAG, "checkPermission()");
+        //Ask for permission if it wasn't granted yet
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    //Method that asks for permission if not already granted
+    private void askPermission() {
+        Log.d(TAG, "askPermission()");
+        ActivityCompat.requestPermissions(
+                this,
+                new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+                REQ_PERMISSION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQ_PERMISSION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //User has granted permission
+                    getLastKnownLocation();
+                } else {
+                    //Permission denied
+                    permissionDenied();
+                }
+                break;
+            }
+        }
+    }
+
+    //App should not continue to function without the permissions
+    private void permissionDenied() {
+        Log.w(TAG, "permissionDenied()");
     }
 }
